626079d6ab593f2f673cf0717d371fe1fda79850
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2023-04-09 16:50:26 +0530

    Update pipwizard.py

--- pipwizard.py
+++ pipwizard.py
@@ -1,12 +1,31 @@
 import importlib
 import subprocess
 
-def install_lib (pkg):
-    try:
-        importlib.import_module(pkg)
-        print(f"{pkg} is already installed")
-    except ImportError:
-        subprocess.check_call(["pip", "install", pkg])
-        print(f"{pkg} had been installed.")
+print('''
+(1) Install library
+(2) Upgrade library
+(3) Uninstall library''')
+option = input("Enter opton: ")
 
-install_lib("pyperclip")
+if option == "1":
+    print()
+    install_lib = input("Enter library name: ")
+
+    try:
+        importlib.import_module(install_lib)
+        print(f"{install_lib} is already installed")
+    except ImportError:
+        subprocess.check_call(["pip", "install", install_lib])
+        print(f"{install_lib} had been installed.")
+
+elif option == "2":
+    upgrade_lib = input("Enter library name: ")
+
+    subprocess.check_call(["pip", "install", "--upgrade", upgrade_lib])
+    print(f"{upgrade_lib} has been upgraded.")
+
+elif option == "3":
+    uninstall_lib = input("Enter library name: ")
+
+    subprocess.check_call(["pip", "uninstall", uninstall_lib])
+    print(f"{uninstall_lib} had been uninstalled.")
