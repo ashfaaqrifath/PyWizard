9bfd7eec65c41ab3d174b8dde9adeddcd0803d53
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2023-07-10 00:48:19 +0530

    Update and rename pipwizard.py to pywizard.py

--- pywizard.py
+++ pywizard.py
@@ -16,12 +16,13 @@ colorama.init(autoreset=True)
 #clears terminal
 os.system("cls")
 print('''
- █▀▀█ ▀█▀ █▀▀█  █   █ ▀█▀  █▀▀▀█  █▀▀█  █▀▀█  █▀▀▄ 
- █▄▄█  █  █▄▄█  █ █ █  █   ▄▄▄▀▀  █▄▄█  █▄▄▀  █  █ 
- █    ▄█▄ █     █▄▀▄█ ▄█▄  █▄▄▄█  █  █  █  █  █▄▄▀ v1.5.4''')
-print(Fore.YELLOW + "              PYTHON PACKAGE MANAGER")
+ █▀▀█ █   █ █   █ ▀█▀ █▀▀▀█ █▀▀█ █▀▀█ █▀▀▄ 
+ █▄▄█ █▄▄▄█ █ █ █  █  ▄▄▄▀▀ █▄▄█ █▄▄▀ █  █ 
+ █      █   █▄▀▄█ ▄█▄ █▄▄▄█ █  █ █  █ █▄▄▀ v1.5.4''')
+print(Fore.YELLOW + "          PYTHON PACKAGE MANAGER")
 print()
-print(Fore.LIGHTBLACK_EX + "         Copyright © 2023 Ashfaaq Rifath")
+print(Fore.LIGHTBLACK_EX + "        Copyright © Ashfaaq Rifath")
+
 print('''
  (1) Install package
  (2) Batch install from requirements file
@@ -33,7 +34,8 @@ print('''
  (8) Save requirements file
  (9) Upgrade PIP version
  (10) Check PIP version
- (11) Check Python version''')
+ (11) Check Python version
+ (12) Create EXE for Python file''')
 
 while True:
     print()
@@ -45,28 +47,26 @@ while True:
         try:
             subprocess.check_call(["pip", "install", install_pkg])
             print(" " + Fore.BLACK + Back.GREEN + f" Installed {install_pkg} ")
-            #input(" Press Enter to exit...")
         except subprocess.CalledProcessError:
             print(" " + Fore.BLACK  + Back.RED + " PACKAGE NOT FOUND ")
-            #input(" Press Enter to exit...")
-        #break
 
     elif option == "2":
         print()
-        print(Fore.YELLOW + " NOTE: Make sure requirements.txt is in this directory.")
-        print(Fore.GREEN + " Batch installing packages...")
+        print(Fore.YELLOW + " NOTE: Make sure requirements.txt is in the root directory.")
+        if os.path.isfile("requirements.txt"):
+            print(Fore.GREEN + " Batch installing packages...")
 
-        with open("requirements.txt") as f:
-            reqs = f.read().splitlines()
-        for pkg in reqs:
-            try:
-                subprocess.check_call(["pip", "install", pkg])
-                print(" " + Fore.BLACK + Back.GREEN + f" Installed {pkg} ")
-                print()
-            except subprocess.CalledProcessError:
-                print(" " + Fore.BLACK  + Back.RED + " ERROR OCCURRED ")
-        #input(" Press Enter to exit...")
-        #break
+            with open("requirements.txt") as f:
+                reqs = f.read().splitlines()
+            for pkg in reqs:
+                try:
+                    subprocess.check_call(["pip", "install", pkg])
+                    print(" " + Fore.BLACK + Back.GREEN + f" Installed {pkg} ")
+                    print()
+                except subprocess.CalledProcessError:
+                    print(" " + Fore.BLACK  + Back.RED + " ERROR OCCURRED ")
+        else:
+            print(" " + Fore.BLACK  + Back.RED + " REQUIREMENTS FILE DOES NOT EXIST ")
 
     elif option == "3":
         print()
@@ -75,11 +75,8 @@ while True:
         try:
             subprocess.check_call(["pip", "install", "--upgrade", update_pkg])
             print(" " + Fore.BLACK + Back.GREEN + f" Updated {update_pkg} ")
-            #input(" Press Enter to exit...")
         except subprocess.CalledProcessError:
             print(" " + Fore.BLACK  + Back.RED + " PACKAGE NOT FOUND ")
-            #input(" Press Enter to exit...")
-        #break
 
     elif option == "4":
         print()
@@ -90,54 +87,47 @@ while True:
             print(" " + Fore.BLACK  + Back.RED + f" Uninstalled {uninstall_pkg} ")
         except subprocess.CalledProcessError:
             print(" " + Fore.BLACK  + Back.RED + " PACKAGE NOT FOUND ")
-            #input(" Press Enter to exit...")
-        #break
 
     elif option == "5":
-        confirm = input(Fore.LIGHTRED_EX + " Do you want to proceed? (Y/N): ")
+        confirm = input(Fore.LIGHTRED_EX + " Do you want to proceed? (y/n): ")
         if confirm == "y".lower():
             print()
             print(Fore.YELLOW + " NOTE: Input package names in the requirements file." + Style.RESET_ALL)
-            print(Fore.RED + " Batch uninstalling packages..." + Style.RESET_ALL)
-            print()
+            if os.path.isfile("requirements.txt"):
+                print(Fore.RED + " Batch uninstalling packages..." + Style.RESET_ALL)
+                print()
 
-            with open("requirements.txt") as f:
-                reqs = f.read().splitlines()
-            for pkg in reqs:
-                try:
-                    subprocess.check_call(["pip", "uninstall", "-y", pkg])
-                    print(" " + Fore.BLACK + Back.RED + f" Uninstalled {pkg} ")
-                except subprocess.CalledProcessError:
-                    print(" " + Fore.BLACK  + Back.RED + " PACKAGE NOT FOUND ")
+                with open("requirements.txt") as f:
+                    reqs = f.read().splitlines()
+                for pkg in reqs:
+                    try:
+                        subprocess.check_call(["pip", "uninstall", "-y", pkg])
+                        print(" " + Fore.BLACK + Back.RED + f" Uninstalled {pkg} ")
+                    except subprocess.CalledProcessError:
+                        print(" " + Fore.BLACK  + Back.RED + " PACKAGE NOT FOUND ")
+            else:
+                print(" " + Fore.BLACK  + Back.RED + " REQUIREMENTS FILE DOES NOT EXIST ")
         else:
             print(" " + Fore.BLACK  + Back.RED + " UNINSTALL CANCELLED ")
 
     elif option == "6":
         print()
-        verify_pkg = input(Fore.YELLOW + " Enter package name: " + Style.RESET_ALL)
+        verify_pkg = input(Fore.CYAN + " Enter package name: " + Style.RESET_ALL)
 
         try:
             subprocess.check_call(["pip", "show", verify_pkg])
             print(" " + Fore.BLACK  + Back.GREEN + f" {verify_pkg} package exists ")
-            #input(" Press Enter to exit...")
         except subprocess.CalledProcessError:
             print(" " + Fore.BLACK  + Back.RED + " PACKAGE NOT FOUND ")
-            #input(" Press Enter to exit...")
-        #break
 
     elif option == "7":
         print()
         print(Fore.GREEN + " Displaying all installed packages...")
         subprocess.check_call(["pip", "list"])
-
-        # run pip list command and capture output
-        output = subprocess.check_output(["pip", "list"])
-        with open("packages.txt", "w") as f:
-            f.write(output.decode("utf-8"))
-
+        
     elif option == "8":
         print()
-        print(Fore.YELLOW + " NOTE: Copy this tool to the project folder ")
+        print(Fore.YELLOW + " NOTE: Run this tool from the desired project folder.")
         print(Fore.GREEN + " Saving project requirements to text file...")
 
         try:
@@ -168,6 +158,48 @@ while True:
         print(Fore.GREEN + " Displaying Python version")
         subprocess.run(["python", "--version"])
 
+    elif option == "12":
+        if pkgutil.find_loader("pyinstaller") is None:
+            subprocess.check_call(["pip", "install", "pyinstaller"])
+        #installs pyinstaller automatically
+        else:
+            pass
+
+        print()
+        file_name = input(Fore.CYAN + " Enter Python file name (.py): " + Style.RESET_ALL)
+
+        if os.path.isfile(file_name):
+            icon_option = input(Fore.YELLOW + " Do you want an icon for this file? (y/n): " + Style.RESET_ALL)
+
+            if icon_option == "y".lower():
+                icon_name = input(Fore.CYAN + " Enter ICO file name (.ico): " + Style.RESET_ALL)
+
+                if os.path.isfile(icon_name):
+                    print()
+                    print(Fore.YELLOW + " NOTE: Run this tool from the desired project folder.")
+                    print(Fore.GREEN + f" Creating EXE file for {file_name}...")
+
+                    try:
+                        subprocess.run(["pyinstaller", "--onefile", f"--icon={icon_name}", file_name])
+                        print(" " + Fore.BLACK  + Back.GREEN + f" Created EXE file for {file_name} ")
+                    except subprocess.CalledProcessError:
+                        print(" " + Fore.BLACK  + Back.RED + " ERROR OCCURRED ")
+                else:
+                    print(" " + Fore.BLACK  + Back.RED + " FILE DOES NOT EXIST ")
+
+            if icon_option == "n".lower():
+                print()
+                print(Fore.YELLOW + " NOTE: Run this tool from the desired project folder.")
+                print(Fore.GREEN + f" Creating EXE file for {file_name}...")
+
+                try:
+                    subprocess.run(["pyinstaller", "--onefile", file_name])
+                    print(" " + Fore.BLACK  + Back.GREEN + f" Created EXE file for {file_name} ")
+                except subprocess.CalledProcessError:
+                    print(" " + Fore.BLACK  + Back.RED + " ERROR OCCURRED ")
+        else:
+            print(" " + Fore.BLACK  + Back.RED + " FILE DOES NOT EXIST ")
+
     elif option == "help":
         print('''
  (1) Install package
@@ -180,7 +212,12 @@ while True:
  (8) Save requirements file
  (9) Upgrade PIP version
  (10) Check PIP version
- (11) Check Python version''')
+ (11) Check Python version
+ (12) Create EXE for Python file''')
 
     else:
         print(" " + Fore.BLACK  + Back.RED + " INVALID OPTION ")
+
+
+
+# Copyright © 2023 Ashfaaq Rifath - PyWizard v1.5.4
